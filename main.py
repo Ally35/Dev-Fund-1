1e1e1bccdb3e12218bb2262414dabe4db89a8c63
Ally35 <allyson.engle@yahoo.com>
2020-05-16 03:26:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Operator Precedence")
+print(20 + 3 * 4)
+
+# ()
+# **
+print((2 - 3) + 2 ** 3)
+# * /
+# + -
+#PEMDAS
+
+print("Bin and Complex Nums")
+print(bin(5))
+print(int("0b101", 2))
+
+print("Variable")
+
+iq = 190
+print(iq)
+#snake_case
+#start with lower case or underscore
+#Letters, numbers, underscore
+#Case sensitive
+#Don't overwrite keywords
+
